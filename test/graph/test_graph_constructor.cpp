9b7022f893d699602524410955b06ba2bc8a13e8
z5206032 <z5206032@cse.unsw.edu.au>
2021-08-01 01:01:16 +1000

    Write initialiser list sections

--- test/graph/test_graph_constructor.cpp
+++ test/graph/test_graph_constructor.cpp
@@ -2,6 +2,7 @@
 
 #include <catch2/catch.hpp>
 
+#include <initializer_list>
 #include <iostream>
 
 TEST_CASE("No-parameters constructor should initialise empty graph") {
@@ -43,18 +44,88 @@ TEST_CASE("Initialiser-list constructor should create graph with values from ini
 		CHECK(g.is_node(35.1231) == true);
 		CHECK(g.is_node(-5.4) == true);
 	}
-	SECTION("Initialiser list with ints") {}
-	SECTION("Initialiser list with chars") {}
-	SECTION("Initialiser list with bools") {}
-	SECTION("Initialiser list with strings") {}
-	SECTION("Initialiser list with doubles") {}
+
+	SECTION("Initialiser list with chars") {
+		auto g = gdwg::graph<char, std::string>({'3', 'c', 'C', ' ', '\n', '\0', 'q', '~'});
+		CHECK(g.empty() == false);
+		CHECK(g.is_node('3') == true);
+		CHECK(g.is_node('c') == true);
+		CHECK(g.is_node('C') == true);
+		CHECK(g.is_node(' ') == true);
+		CHECK(g.is_node('\n') == true);
+		CHECK(g.is_node('\0') == true);
+		CHECK(g.is_node('q') == true);
+		CHECK(g.is_node('~') == true);
+	}
+
+	SECTION("Initialiser list with strings") {
+		auto g = gdwg::graph<std::string, std::string>({"kj3n2kr1", "", "2\0mk", "\""});
+		CHECK(g.empty() == false);
+		CHECK(g.is_node("kj3n2kr1") == true);
+		CHECK(g.is_node("") == true);
+		CHECK(g.is_node("2\0mk") == true);
+		CHECK(g.is_node("\"") == true);
+	}
 }
 
 TEST_CASE("Iterator constructor should create graph with [start, end) values") {
-	SECTION("Initialiser list iterators for a full range") {}
-	SECTION("Initialiser list iterators for a subrange including start and excluding end") {}
-	SECTION("Initialiser list iterators for a subrange excluding start and including end") {}
-	SECTION("Initialiser list iterators for a subrange excluding start and excluding end") {}
+	SECTION("Initialiser list iterators for an empty range") {
+		auto il = std::initializer_list<int>();
+		auto g = gdwg::graph<int, double>(il.begin(), il.end());
+		CHECK(g.empty() == true);
+	}
+
+	SECTION("Initialiser list iterators for a full range") {
+		auto il = {300000000, 5, 1, 912, -4, -335, -1, 0, 81};
+		auto g = gdwg::graph<int, double>(il.begin(), il.end());
+		CHECK(g.empty() == false);
+		CHECK(g.is_node(300000000) == true);
+		CHECK(g.is_node(5) == true);
+		CHECK(g.is_node(1) == true);
+		CHECK(g.is_node(912) == true);
+		CHECK(g.is_node(-4) == true);
+		CHECK(g.is_node(-335) == true);
+		CHECK(g.is_node(-1) == true);
+		CHECK(g.is_node(0) == true);
+		CHECK(g.is_node(81) == true);
+	}
+
+	SECTION("Initialiser list iterators for a subrange including start and excluding end") {
+		auto il = {"first", "second", "third", "fourth", "fifth", "sixth"};
+		auto g = gdwg::graph<std::string, double>(il.begin(), il.begin() + 3);
+		CHECK(g.empty() == false);
+		CHECK(g.is_node("first") == true);
+		CHECK(g.is_node("second") == true);
+		CHECK(g.is_node("third") == true);
+		CHECK(g.is_node("fourth") == false);
+		CHECK(g.is_node("fifth") == false);
+		CHECK(g.is_node("sixth") == false);
+	}
+
+	SECTION("Initialiser list iterators for a subrange excluding start and including end") {
+		auto il = {"first", "second", "third", "fourth", "fifth", "sixth"};
+		auto g = gdwg::graph<std::string, double>(il.begin() + 4, il.end());
+		CHECK(g.empty() == false);
+		CHECK(g.is_node("first") == false);
+		CHECK(g.is_node("second") == false);
+		CHECK(g.is_node("third") == false);
+		CHECK(g.is_node("fourth") == false);
+		CHECK(g.is_node("fifth") == true);
+		CHECK(g.is_node("sixth") == true);
+	}
+
+	SECTION("Initialiser list iterators for a subrange excluding start and excluding end") {
+		auto il ={"first", "second", "third", "fourth", "fifth", "sixth"};
+		auto g = gdwg::graph<std::string, double>(il.begin() + 2, il.begin() + 5);
+		CHECK(g.empty() == false);
+		CHECK(g.is_node("first") == false);
+		CHECK(g.is_node("second") == false);
+		CHECK(g.is_node("third") == true);
+		CHECK(g.is_node("fourth") == true);
+		CHECK(g.is_node("fifth") == true);
+		CHECK(g.is_node("sixth") == false);
+	}
+
 	SECTION("Array iterators for some range") {}
 	SECTION("Stack iterators for some range") {}
 	SECTION("Queue iterators for some range") {}
