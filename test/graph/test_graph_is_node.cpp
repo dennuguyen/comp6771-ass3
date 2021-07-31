ad78cf1e4d64917477dd69de8a635721ec3f72a2
z5206032 <z5206032@cse.unsw.edu.au>
2021-07-31 23:11:35 +1000

    Skeleton test cases and sections

--- test/graph/test_graph_is_node.cpp
+++ test/graph/test_graph_is_node.cpp
@@ -2,9 +2,24 @@
 
 #include <catch2/catch.hpp>
 
-TEST_CASE("basic test") {
-	auto g = gdwg::graph<int, std::string>{};
-	auto n = 5;
-	g.insert_node(n);
-	CHECK(g.is_node(n));
+TEST_CASE("is_node() should return true if there is a node with same value_type in the graph") {
+	SECTION("Graph with 1 node") {}
+	SECTION("Graph with 10 nodes") {}
+	SECTION("Graph with cycle of nodes") {}
+	SECTION("Graph with tree of nodes") {}
+	SECTION("Graph with tree of nodes and edges") {}
+	SECTION("Graph with forests of nodes and edges") {}
+	SECTION("Node has only outgoing edges") {}
+	SECTION("Node has only incoming edges") {}
+	SECTION("Node has only both incoming and outgoing edges") {}
+	SECTION("Node is isolated from graph") {}
+}
+
+TEST_CASE("is_node() should return false if there is no node with same value_type in graph") {
+	SECTION("Empty graph") {}
+	SECTION("Graph with 1 node") {}
+	SECTION("Graph with 10 nodes") {}
+	SECTION("Graph with cycle of nodes") {}
+	SECTION("Graph with tree of nodes and edges") {}
+	SECTION("Graph with forests of nodes and edges") {}
 }
