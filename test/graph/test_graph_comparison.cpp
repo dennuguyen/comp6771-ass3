c389e6be4d92f7770ccc9fed3fb9ca3f871081ba
z5206032 <z5206032@cse.unsw.edu.au>
2021-08-02 05:22:44 +1000

    Write test cases

--- test/graph/test_graph_comparison.cpp
+++ test/graph/test_graph_comparison.cpp
@@ -2,18 +2,50 @@
 
 #include <catch2/catch.hpp>
 
-TEST_CASE("Comparing two graphs with same value_type for all nodes and edges should return true") {
-	SECTION("Empty graph") {}
-	SECTION("Graph with only nodes") {}
-	SECTION("Graph with nodes and edges") {}
-	SECTION("Graph with cycles") {}
-	SECTION("Graph with trees") {}
+TEST_CASE("Comparing two graphs with the same nodes and edges should return true") {
+	SECTION("Empty graph") {
+		auto g1 = gdwg::graph<std::string, int>();
+		auto g2 = gdwg::graph<std::string, int>();
+		CHECK(g1 == g2);
+	}
+
+	SECTION("Graph with only nodes") {
+		auto g1 = gdwg::graph<std::string, int>({"A", "B", "C"});
+		auto g2 = gdwg::graph<std::string, int>(g1);
+		CHECK(g1 == g2);
+	}
+
+	SECTION("Graph with nodes and edges") {
+		auto g1 = gdwg::graph<std::string, int>({"A", "B", "C"});
+		g1.insert_edge("A", "B", 1);
+		g1.insert_edge("A", "C", 1);
+		g1.insert_edge("C", "B", 1);
+		auto g2 = gdwg::graph<std::string, int>(g1);
+		CHECK(g1 == g2);
+	}
 }
 
-TEST_CASE("Comparing two graphs with any non-equal value_types should return false") {
-	SECTION("Empty graph and graph with a node") {}
-	SECTION("Graph with only nodes") {}
-	SECTION("Graph with nodes and edges") {}
-	SECTION("Graph with cycles") {}
-	SECTION("Graph with trees") {}
+TEST_CASE("Comparing two graphs with any non-equal nodes and edges should return false") {
+	SECTION("Empty graph and graph with a node") {
+		auto g1 = gdwg::graph<std::string, int>();
+		auto g2 = gdwg::graph<std::string, int>({"A"});
+		CHECK(g1 != g2);
+	}
+
+	SECTION("Graph with only nodes") {
+		auto g1 = gdwg::graph<std::string, int>({"A", "B", "C"});
+		auto g2 = gdwg::graph<std::string, int>({"A", "E", "C"});
+		CHECK(g1 != g2);
+	}
+
+	SECTION("Graph with nodes and edges") {
+		auto g1 = gdwg::graph<std::string, int>({"A", "B", "C"});
+		g1.insert_edge("A", "B", 1);
+		g1.insert_edge("A", "C", 1);
+		g1.insert_edge("C", "B", 1);
+		auto g2 = gdwg::graph<std::string, int>(g1);
+		g2.insert_edge("A", "B", 2);
+		g2.erase_edge("A", "C", 1);
+		CHECK(g1 != g2);
+	}
 }
