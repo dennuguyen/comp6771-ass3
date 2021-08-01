f4ecc468c00a0a470a8cce73807be8388e23feb5
z5206032 <z5206032@cse.unsw.edu.au>
2021-08-01 14:16:07 +1000

    Write test cases

--- test/graph/test_graph_empty.cpp
+++ test/graph/test_graph_empty.cpp
@@ -3,16 +3,21 @@
 #include <catch2/catch.hpp>
 
 TEST_CASE("Empty should be true if there are no nodes and edges in the graph") {
-	SECTION("Empty graph") {}
+	SECTION("Empty graph") {
+		auto g = gdwg::graph<std::string, std::string>();
+		CHECK(g.empty() == true);
+	}
 }
 
 TEST_CASE("Empty should be false if there are any nodes in the graph") {
-	SECTION("Graph with 1 node") {}
-	SECTION("Graph with 10 nodes") {}
-}
+	SECTION("Graph with 1 node") {
+		auto g = gdwg::graph<std::string, std::string>({""});
+		CHECK(g.empty() == false);
+	}
 
-TEST_CASE("Empty should be false if there are any nodes and edges in the graph") {
-	SECTION("Graph with 1 self-directed node") {}
-	SECTION("Graph with only a cycle") {}
-	SECTION("Graph with only a single-directed tree") {}
+	SECTION("Graph with 10 nodes") {
+		auto g = gdwg::graph<std::string, std::string>(
+		   {"1923", "2532", "5433", "4862", "9995", "6649", "7101", "8234", "9293", "1620"});
+		CHECK(g.empty() == false);
+	}
 }
