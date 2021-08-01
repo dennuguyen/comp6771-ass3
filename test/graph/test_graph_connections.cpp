071c5abc0df39667a2fba5500b6d781ee55d39f8
z5206032 <z5206032@cse.unsw.edu.au>
2021-08-02 03:13:19 +1000

    Write test cases

--- test/graph/test_graph_connections.cpp
+++ test/graph/test_graph_connections.cpp
@@ -1,20 +1,51 @@
 #include "gdwg/graph.hpp"
 
 #include <catch2/catch.hpp>
+#include <stdexcept>
 
-TEST_CASE("Connections should return a set of nodes in ascending order from a given node") {
-	SECTION("Graph with node with no edges") {}
-	SECTION("Graph with 1 node") {}
-	SECTION("Graph with 1 node with 1 self-directed edge") {}
-	SECTION("Graph with 1 node with many self-directed edges") {}
-	SECTION("Graph with a few immediate nodes") {}
-	SECTION("Graph with nodes a few levels deep") {}
-	SECTION("Graph with cycles") {}
-	SECTION("Graph with trees") {}
-	SECTION("Graph with forests") {}
+TEST_CASE("Connections should return a outgoing nodes in ascending order from a given node") {
+	SECTION("Graph with 1 node and no edges") {
+		auto g = gdwg::graph<std::string, int>({"A"});
+		CHECK(g.connections("A") == std::vector<std::string>({}));
+	}
+
+	SECTION("Graph with 1 node with some self-directed edges") {
+		auto g = gdwg::graph<std::string, int>({"A"});
+		g.insert_edge("A", "A", 4);
+		g.insert_edge("A", "A", 1);
+		g.insert_edge("A", "A", 3);
+		g.insert_edge("A", "A", 2);
+		CHECK(g.connections("A") == std::vector<std::string>({"A"}));
+	}
+
+	SECTION("Graph with a few immediate nodes") {
+		auto g = gdwg::graph<std::string, int>({"A", "B", "C"});
+		g.insert_edge("A", "A", 4);
+		g.insert_edge("A", "B", 1);
+		g.insert_edge("A", "C", 3);
+		g.insert_edge("A", "A", 2);
+		CHECK(g.connections("A") == std::vector<std::string>({"A", "B", "C"}));
+	}
+
+	SECTION("Graph with incoming/outgoing nodes") {
+		auto g = gdwg::graph<std::string, int>({"A", "B", "C"});
+		g.insert_edge("A", "A", 4);
+		g.insert_edge("B", "B", 1);
+		g.insert_edge("A", "C", 3);
+		g.insert_edge("C", "A", 2);
+		g.insert_edge("B", "A", 0);
+		CHECK(g.connections("A") == std::vector<std::string>({"A", "C"}));
+	}
 }
 
 TEST_CASE("Calling connections with non-existent node should throw") {
-	SECTION("Empty graph") {}
-	SECTION("Invalid node") {}
+	SECTION("Empty graph") {
+		auto g = gdwg::graph<std::string, int>();
+		CHECK_THROWS_AS(g.connections("A"), std::runtime_error);
+	}
+
+	SECTION("Invalid node") {
+		auto g = gdwg::graph<std::string, int>({"A", "B", "C"});
+		CHECK_THROWS_AS(g.connections("D"), std::runtime_error);
+	}
 }
