56b4b524a9b67462586051470559bb58bf5f68f2
z5206032 <z5206032@cse.unsw.edu.au>
2021-08-01 22:28:37 +1000

    Write test cases

--- test/graph/test_graph_merge_replace_node.cpp
+++ test/graph/test_graph_merge_replace_node.cpp
@@ -1,10 +1,45 @@
 #include "gdwg/graph.hpp"
 
 #include <catch2/catch.hpp>
+#include <stdexcept>
 
-TEST_CASE("basic test") {
-	auto g = gdwg::graph<int, std::string>{};
-	auto n = 5;
-	g.insert_node(n);
-	CHECK(g.is_node(n));
+TEST_CASE("Merge-replacing a node should replace the node's value and merge duplicated edges") {
+	SECTION("Simple merge replacement") {
+		auto g = gdwg::graph<std::string, int>({"A", "B", "C", "D"});
+		g.insert_edge("A", "B", 1);
+		g.insert_edge("A", "C", 2);
+		g.insert_edge("A", "D", 3);
+		g.merge_replace_node("A", "B");
+		CHECK(g.find("B", "B", 1) != g.end());
+		CHECK(g.find("B", "C", 2) != g.end());
+		CHECK(g.find("B", "D", 3) != g.end());
+	}
+
+	SECTION("Merge replacement with duplicate edges") {
+		auto g = gdwg::graph<std::string, int>({"A", "B", "C", "D"});
+		g.insert_edge("A", "B", 1);
+		g.insert_edge("A", "C", 2);
+		g.insert_edge("A", "D", 3);
+		g.merge_replace_node("A", "B");
+		CHECK(g.find("B", "B", 1) != g.end());
+		CHECK(g.find("B", "C", 2) != g.end());
+		CHECK(g.find("B", "D", 3) != g.end());
+	}
+}
+
+TEST_CASE("Merge-replacing a node without an existing node to replace should throw") {
+	SECTION("Empty graph") {
+		auto g = gdwg::graph<std::string, int>();
+		CHECK_THROWS_AS(g.replace_node("A", "B"), std::runtime_error);
+	}
+
+	SECTION("Non-existent source node") {
+		auto g = gdwg::graph<std::string, int>({"A", "B"});
+		CHECK_THROWS_AS(g.replace_node("C", "A"), std::runtime_error);
+	}
+
+	SECTION("Non-existent destination node") {
+		auto g = gdwg::graph<std::string, int>({"A", "B"});
+		CHECK_THROWS_AS(g.replace_node("B", "C"), std::runtime_error);
+	}
 }
--- test/graph/test_graph_replace_node.cpp
+++ test/graph/test_graph_replace_node.cpp
@@ -3,7 +3,7 @@
 #include <catch2/catch.hpp>
 #include <stdexcept>
 
-TEST_CASE("Replacing a node should replace the node's data") {
+TEST_CASE("Replacing a node should replace the node's value") {
 	SECTION("Replacing node with string data type") {
 		auto g = gdwg::graph<std::string, int>({"A"});
 		g.replace_node("A", "B");
@@ -14,6 +14,15 @@ TEST_CASE("Replacing a node should replace the node's data") {
 		auto g = gdwg::graph<int, int>({3});
 		g.replace_node(3, 4);
 		CHECK(g.nodes() == std::vector<int>({4}));
+	}
+
+	SECTION("Replacing node with edges") {
+		auto g = gdwg::graph<std::string, int>({"A", "B", "C"});
+		g.insert_edge("A", "B", 1);
+		g.insert_edge("A", "C", 2);
+		g.replace_node("A", "D");
+		CHECK(g.find("D", "B", 1) != g.end());
+		CHECK(g.find("D", "C", 2) != g.end());
 	}
 }
 
@@ -26,6 +35,13 @@ TEST_CASE("Successfully replacing a node should return true") {
 	SECTION("Replacing node with integer data type") {
 		auto g = gdwg::graph<int, int>({3});
 		CHECK(g.replace_node(3, 4) == true);
+	}
+
+	SECTION("Replacing node with edges") {
+		auto g = gdwg::graph<std::string, int>({"A", "B", "C"});
+		g.insert_edge("A", "B", 1);
+		g.insert_edge("A", "C", 2);
+		CHECK(g.replace_node("A", "D") == true);
 	}
 }
 
